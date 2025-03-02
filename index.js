faf520257d9d590fc46258a20e1b00bca86feb38
= <=>
2025-03-02 22:44:39 +0530

    added env vars

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
@@ -18,4 +21,4 @@ app.use(express.static("./public"));
 app.get("/", (req, res) => {
   return res.sendFile("/public/index.html");
 });
-server.listen(3000, () => console.log(`server started at port 3000`));
+server.listen(PORT, () => console.log(`server started at port 3000`));
